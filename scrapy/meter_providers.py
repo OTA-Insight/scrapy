c0e922d1a3180f5632eea5c443a19913ddd0f702
brecht stamper <stamper.brecht@gmail.com>
2023-07-25 14:26:01 +0200

    MeterProvider classes

---
+++ scrapy/meter_providers.py
@@ -0,0 +1,31 @@
+"""
+Scrapy extension for collecting otlp metrics
+"""
+from abc import ABC, abstractmethod
+import logging
+from typing import TYPE_CHECKING
+
+from opentelemetry.metrics import NoOpMeterProvider, MeterProvider
+
+if TYPE_CHECKING:
+    from scrapy.crawler import Crawler
+
+logger = logging.getLogger(__name__)
+
+
+class MeterProvider(MeterProvider, ABC):
+    """
+    Wrapper for MeterProvider so we have crawler available during init.
+    """
+    @abstractmethod
+    def __init__(self, crawler: "Crawler"):
+        """
+        Use crawler to configure the Meter Provider
+        """
+
+class NoOpMeterProvider(NoOpMeterProvider):
+    """
+    Wrapper for NoOpMeterProvider so we support passing crawler during init.
+    """
+    def __init__(self, crawler: "Crawler"):
+        super().__init__()
